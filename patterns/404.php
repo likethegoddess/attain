e279ce9859a901e1aab0f38cb5ee831a1ab0d2e9
Diana Thompson <likethegoddess@users.noreply.github.com>
2023-04-28 18:41:50 -0700

    Add 404 pattern

---
+++ patterns/404.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: 404
+ * Slug: afsey/404
+ * Inserter: no
+ */
+?>
+<!-- wp:spacer {"height":"var(--wp--preset--spacing--30)"} -->
+<div style="height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"level":1} -->
+<h1><?php echo esc_html_e( '404', 'Error code for a webpage that is not found.', 'afsey' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"default"},"style":{"spacing":{"margin":{"top":"5px"}}}} -->
+<div class="wp-block-group" style="margin-top:5px">
+	<!-- wp:paragraph -->
+	<p><?php echo esc_html_e( 'This page could not be found. Maybe try a search?', 'Message to convey that a webpage could not be found', 'afsey' ); ?></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:search {"label":"<?php echo esc_html_e( 'Search', 'label', 'afsey' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'afsey' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'afsey' ); ?>","buttonUseIcon":true,"align":"center"} /-->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"var(--wp--preset--spacing--70)"} -->
+<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
