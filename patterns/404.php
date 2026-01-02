01853fed2b282fc082e39a23851afe29347c632e
DAnn2012 <dannunziata@hotmail.it>
2026-01-02 14:41:18 +0100

    Update 404.php

--- patterns/404.php
+++ patterns/404.php
@@ -10,16 +10,16 @@
 <!-- /wp:spacer -->
 
 <!-- wp:heading {"level":1} -->
-<h1><?php esc_html_e( '404', 'Error code for a webpage that is not found.', 'attain' ); ?></h1>
+<h1><?php echo esc_html_x( '404', 'Error code for a webpage that is not found.', 'attain' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:group {"layout":{"type":"default"},"style":{"spacing":{"margin":{"top":"5px"}}}} -->
 <div class="wp-block-group" style="margin-top:5px">
 	<!-- wp:paragraph -->
-	<p><?php esc_html_e( 'This page could not be found. Maybe try a search?', 'Message to convey that a webpage could not be found', 'attain' ); ?></p>
+	<p><?php echo esc_html_x( 'This page could not be found. Maybe try a search?', 'Message to convey that a webpage could not be found', 'attain' ); ?></p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'label', 'attain' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'attain' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'attain' ); ?>","buttonUseIcon":true,"align":"center"} /-->
+	<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'label', 'attain' ); ?>","placeholder":"<?php echo esc_attr_x( 'Search...', 'placeholder for search field', 'attain' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'attain' ); ?>","buttonUseIcon":true,"align":"center"} /-->
 </div>
 <!-- /wp:group -->
 
